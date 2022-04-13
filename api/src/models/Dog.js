42b8219b147ca05974c77b9c47974f13c66b8a55
Johan Tovar~ <jtovarto@gmail.com>
2022-04-12 23:09:15 -0400

    feat: :sparkles: add Dog model

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,35 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+const { nanoid } = require("nanoid");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define("dog", {
+    id: {
+      type: DataTypes.STRING,
+      autoIncrement: false,
+      primaryKey: true,
+      defaultValue: nanoid(),
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lifespan: {
+      type: DataTypes.STRING,
+      defaultValue: "1 - 10",
+      set(value) {
+        this.setDataValue("lifespan", value + " years");
+      },
     },
   });
 };
