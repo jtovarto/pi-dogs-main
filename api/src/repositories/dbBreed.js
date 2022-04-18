b4912aebcc6112b65d655005c2c0c98f48bffc9e
Johan Tovar~ <jtovarto@gmail.com>
2022-04-17 23:33:15 -0400

    feat: :sparkles: add create method in dbBreed repository

--- api/src/repositories/dbBreed.js
+++ api/src/repositories/dbBreed.js
@@ -29,7 +29,7 @@ const getById = async (id) => {
         through: { attributes: [] },
       },
     });
-    
+
     if (results === null) return {};
     return results;
   } catch (error) {
@@ -37,7 +37,50 @@ const getById = async (id) => {
   }
 };
 
+const create = async (value) => {
+  let { name, weight, height, lifespan, image, temperaments } = value;
+
+  if (!name || !height || !weight) {
+    throw new Error("Fields name-height-weight are required");
+  }
+  try {
+    const dog = await Dog.create({
+      name,
+      weight: matrixFormat(weight),
+      height: matrixFormat(height),
+      lifespan: matrixFormat(lifespan ?? [0, 0]),
+      image,
+    });
+
+    const createdTemperaments = await Temperament.findAll({
+      where: { id: temperaments },
+    });
+
+    await dog.setTemperaments(createdTemperaments);
+
+    return dog;
+  } catch (error) {
+    if (error.message === "Validation error") {
+      throw new Error(error.errors[0].message);
+    }
+    throw new Error(error.message);
+  }
+};
+
+function matrixFormat(array) {
+  if (array.length < 2) {
+    throw new Error("You must provide a minimum and maximum value");
+  }
+  if (isNaN(array[0]) || isNaN(array[1])) {
+    throw new Error("Fields weight-height-lifespan must be numeric");
+  }
+  if (array[0] > array[1]) {
+    [array[1], array[0]] = [array[0], array[1]];
+  }
+  return `${array[0]} - ${array[1]}`;
+}
 module.exports = {
   getAll,
   getById,
+  create,
 };
