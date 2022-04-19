80f339904a9488fcaf5ed9785097921bac6aafcb
Johan Tovar~ <jtovarto@gmail.com>
2022-04-19 19:24:42 -0400

    feat: add routes to pages created in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Main from "./Pages/Main";
+import Detail from "./Pages/Detail";
+import FormCreate from "./Pages/FormCreate";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/create" element={<FormCreate/>} />
+      </Routes>
     </div>
   );
 }
