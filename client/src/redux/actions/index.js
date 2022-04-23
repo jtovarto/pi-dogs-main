7adf46592812f7c11ab2ed2613bfdc6f8b0df0fc
Johan Tovar~ <jtovarto@gmail.com>
2022-04-22 20:58:06 -0400

    apply redux thunk in actions

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -3,15 +3,16 @@ import axios from "axios";
 export const GET_ALL_BREEDS = "GET_ALL_BREEDS";
 export const GET_ALL_TEMPERAMENTS = "GET_ALL_TEMPERAMENTS";
 
-export function getAllBreeds(dispatch) {
-  return function (name, source) {
-    let query;
+export function getAllBreeds(name, source) {
+  return function (dispatch) {
+    let query = "";
     if (name) query += "?name=" + name;
     if (source) {
       query ? (query += "&") : (query += "?");
       query += "source=" + source;
     }
-    return axios("http://localhost:3001/dogs")
+    
+    return axios("http://localhost:3001/dogs" + query)
       .then((response) => {
         dispatch({
           type: GET_ALL_BREEDS,
@@ -22,8 +23,8 @@ export function getAllBreeds(dispatch) {
   };
 }
 
-export function getAllTemperaments(dispatch) {
-  return function () {  
+export function getAllTemperaments() {
+  return function (dispatch) {
     return axios("http://localhost:3001/temperament")
       .then((response) => {
         dispatch({
@@ -34,3 +35,10 @@ export function getAllTemperaments(dispatch) {
       .catch((err) => console.log(err));
   };
 }
+
+export const getBreedsByName = () => {
+  return (dispatch) => {
+    console.log("dispatch");
+    dispatch({ type: GET_ALL_BREEDS });
+  };
+};
