3ca3681cdae0f1e07fcd556d3762683ff965f5ae
Johan Tovar~ <jtovarto@gmail.com>
2022-04-29 13:52:12 -0400

    feat: :sparkles: add dark mode styles

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,22 @@ import Main from "./pages/Main";
 import Detail from "./pages/Detail";
 import FormCreate from "./pages/FormCreate";
 import Notification from "./components/Notifications";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const theme = useSelector((state) => state.theme);
+
   return (
-    <div className="App">
+    <div className={theme}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/main" element={<Main />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/create" element={<FormCreate />} />
       </Routes>
-      
+
       <Notification />
-      
     </div>
   );
 }
---
+++ client/src/hooks/useTheme.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { changeTheme } from "../redux/actions";
+export const THEME_LIGHT = "theme-light";
+export const THEME_DARK = "theme-dark";
+
+export function useTheme() {
+  const dispatch = useDispatch();
+  const [theme, setTheme] = useState(THEME_LIGHT);
+
+  useEffect(() => {
+    console.log("entra");
+    let storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "") {
+      storedTheme = theme;
+      dispatch(changeTheme(theme));
+    }
+
+    if (storedTheme !== theme) {
+      localStorage.setItem("theme", theme);
+      dispatch(changeTheme(theme));
+    }
+  }, [theme, dispatch]);
+
+  const toogleTheme = function () {
+    if (localStorage.getItem("theme") === THEME_DARK) {
+      setTheme(THEME_LIGHT);
+    } else {
+      setTheme(THEME_DARK);
+    }
+  };
+
+  return { toogleTheme };
+}
