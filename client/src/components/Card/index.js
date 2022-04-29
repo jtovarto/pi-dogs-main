74232a652cf7bd3f8657ff026c2a21cf2222f8aa
Johan Tovar~ <jtovarto@gmail.com>
2022-04-29 19:56:52 -0400

    add card component

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,45 @@
+import styles from "./Card.module.css";
+const Card = ({ breed }) => {
+  return (
+    <div
+      className={`${styles.card} ${styles.card}`}
+      style={{ backgroundImage: `url(${breed.image})` }}
+    >
+      <div className={styles.border}>
+        <h2>{breed.name}</h2>
+        <div className={styles.info}>
+          {breed.temperaments.map((temper) => (
+            <span>{temper}</span>
+          ))}
+        </div>
+        <div className={styles.info}>
+            <span>Weight: {breed.weight[0]} - {breed.weight[1]}</span> 
+        </div>
+        <div className={styles.icons}>
+          <i
+            className={`${styles.fa} ${styles.fa_codepen}`}
+            aria-hidden="true"
+          ></i>
+          <i
+            className={`${styles.fa} ${styles.fa_instagram}`}
+            aria-hidden="true"
+          ></i>
+          <i
+            className={`${styles.fa} ${styles.fa_dribbble}`}
+            aria-hidden="true"
+          ></i>
+          <i
+            className={`${styles.fa} ${styles.fa_twitter}`}
+            aria-hidden="true"
+          ></i>
+          <i
+            className={`${styles.fa} ${styles.fa_facebook}`}
+            aria-hidden="true"
+          ></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
