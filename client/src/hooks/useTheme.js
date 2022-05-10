08e169ca1e4118ae4e02bc458b9b0a1405bcdcb3
Johan Tovar~ <jtovarto@gmail.com>
2022-05-10 18:00:48 -0400

    fix: add persistence to dark mode

--- client/src/hooks/useTheme.js
+++ client/src/hooks/useTheme.js
@@ -6,19 +6,21 @@ export const THEME_DARK = "theme-dark";
 
 export function useTheme() {
   const dispatch = useDispatch();
-  const [theme, setTheme] = useState(THEME_LIGHT);
+  const [theme, setTheme] = useState(null);
 
   useEffect(() => {
     let storedTheme = localStorage.getItem("theme");
+    let currentTheme = "";
     if (storedTheme === "") {
-      storedTheme = theme;
-      dispatch(changeTheme(theme));
-    }
-
-    if (storedTheme !== theme) {
+      localStorage.setItem("theme", THEME_LIGHT);
+      currentTheme = THEME_LIGHT;
+    } else if (!theme) {
+      currentTheme = storedTheme;
+    } else {
       localStorage.setItem("theme", theme);
-      dispatch(changeTheme(theme));
+      currentTheme = theme;
     }
+    dispatch(changeTheme(currentTheme));
   }, [theme, dispatch]);
 
   const toogleTheme = function () {
