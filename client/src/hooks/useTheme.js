ffa886399b1a8913a935240e9bd1c1b0c5e96808
Johan Tovar~ <jtovarto@gmail.com>
2022-05-11 23:15:42 -0400

    refactor: :recycle: extract unnecessary code in useTheme hook

--- client/src/hooks/useTheme.js
+++ client/src/hooks/useTheme.js
@@ -6,21 +6,14 @@ export const THEME_DARK = "theme-dark";
 
 export function useTheme() {
   const dispatch = useDispatch();
-  const [theme, setTheme] = useState(null);
+  let storedTheme = localStorage.getItem("theme");
+  const [theme, setTheme] = useState(storedTheme ?? THEME_LIGHT);
 
   useEffect(() => {
-    let storedTheme = localStorage.getItem("theme");
-    let currentTheme = "";
-    if (storedTheme === "") {
-      localStorage.setItem("theme", THEME_LIGHT);
-      currentTheme = THEME_LIGHT;
-    } else if (!theme) {
-      currentTheme = storedTheme;
-    } else {
+    if (theme !== storedTheme) {
       localStorage.setItem("theme", theme);
-      currentTheme = theme;
     }
-    dispatch(changeTheme(currentTheme));
+    dispatch(changeTheme(theme));
   }, [theme, dispatch]);
 
   const toogleTheme = function () {
